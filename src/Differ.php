4bf8428ba401d0278f8842f83d45d94f9d30cef4
VK <belsynth@gmail.com>
2021-04-22 17:00:56 +0300

    Рефакторит src/Differ.php

--- src/Differ.php
+++ src/Differ.php
@@ -4,29 +4,31 @@ namespace Differ\Differ;
 
 use function Functional\sort;
 
-function genDiff(string $firstFile, $secondFile): string
+function genDiff(string $beforeFile, $afterFile): string
 {
-    $firstFileData = decodeFileData(getFileData($firstFile));
-    $secondFileData = decodeFileData(getFileData($secondFile));
-    $firstData = convertBoolValues($firstFileData);
-    $secondData = convertBoolValues($secondFileData);
-    $keys = array_keys(array_merge($firstData, $secondData));
+    $beforeFileData = decodeFileContent(getFileContent($beforeFile));
+    $afterFileData = decodeFileContent(getFileContent($afterFile));
+
+    $before = convertBoolValues($beforeFileData);
+    $after = convertBoolValues($afterFileData);
+
+    $keys = array_keys(array_merge($before, $after));
     $sortedKeys = sort($keys, fn($a, $b) => $a <=> $b);
 
     $result = array_reduce(
         $sortedKeys,
-        function ($acc, $key) use ($firstData, $secondData) {
-            if (array_key_exists($key, $firstData) && array_key_exists($key, $secondData)) {
-                if ($firstData[$key] === $secondData[$key]) {
-                    $acc .= "    $key: $firstData[$key]\n";
+        function ($acc, $key) use ($before, $after) {
+            if (array_key_exists($key, $before) && array_key_exists($key, $after)) {
+                if ($before[$key] === $after[$key]) {
+                    $acc .= "    $key: $before[$key]\n";
                 } else {
-                    $acc .= "  - $key: $firstData[$key]\n";
-                    $acc .= "  + $key: $secondData[$key]\n";
+                    $acc .= "  - $key: $before[$key]\n";
+                    $acc .= "  + $key: $after[$key]\n";
                 }
-            } elseif (array_key_exists($key, $firstData) && !array_key_exists($key, $secondData)) {
-                $acc .= "  - $key: $firstData[$key]\n";
+            } elseif (array_key_exists($key, $before) && !array_key_exists($key, $after)) {
+                $acc .= "  - $key: $before[$key]\n";
             } else {
-                $acc .= "  + $key: $secondData[$key]\n";
+                $acc .= "  + $key: $after[$key]\n";
             }
 
             return $acc;
@@ -34,10 +36,10 @@ function genDiff(string $firstFile, $secondFile): string
         ''
     );
 
-    return "{\n$result}\n";
+    return "{\n$result}";
 }
 
-function getFileData(string $path)
+function getFileContent(string $path): string
 {
     if (file_exists($path) && is_readable($path)) {
         $fileData = file_get_contents($path);
@@ -48,10 +50,9 @@ function getFileData(string $path)
     return $fileData;
 }
 
-function decodeFileData(string $fileData)
+function decodeFileContent(string $fileContent): array
 {
-    $jsonDecodedData = json_decode($fileData, true);
-
+    $jsonDecodedData = json_decode($fileContent, true);
     $jsonError = json_last_error();
     if (json_last_error() !== JSON_ERROR_NONE) {
         throw new \Exception("Invalid json data. $jsonError}");
@@ -60,12 +61,12 @@ function decodeFileData(string $fileData)
     return $jsonDecodedData;
 }
 
-function bool2string(bool $value): string
+function convertBoolValueToString(bool $value): string
 {
     return $value ? 'true' : 'false';
 }
 
 function convertBoolValues(array $array): array
 {
-    return array_map(fn($item) => is_bool($item) ? bool2string($item) : $item, $array);
+    return array_map(fn($value) => is_bool($value) ? convertBoolValueToString($value) : $value, $array);
 }
