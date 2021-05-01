c74f50978b652c793bd9c509d5d59669f7301adf
VK <belsynth@gmail.com>
2021-05-01 12:43:22 +0300

    Добавляет создание AST, форматирование stylish

--- src/Differ.php
+++ src/Differ.php
@@ -9,37 +9,60 @@ use function Functional\sort;
 
 function genDiff(string $beforeFile, $afterFile): string
 {
-    $beforeRawData = parse(getFileExtension($beforeFile), getFileContent($beforeFile));
-    $afterRawData = parse(getFileExtension($afterFile), getFileContent($afterFile));
+    $beforeData = parse(getFileExtension($beforeFile), getFileContent($beforeFile));
+    $afterData = parse(getFileExtension($afterFile), getFileContent($afterFile));
+    $diffTree = getDiffTree($beforeData, $afterData);
+    $result = stylish($diffTree);
 
-    $before = convertBoolValues($beforeRawData);
-    $after = convertBoolValues($afterRawData);
+    return "{\n$result\n}";
+}
 
-    $keys = array_keys(array_merge($before, $after));
+function getDiffTree(object $beforeData, object $afterData): array
+{
+    $keys = array_keys(array_merge((array) $beforeData, (array) $afterData));
     $sortedKeys = sort($keys, fn($a, $b) => $a <=> $b);
 
-    $result = array_reduce(
-        $sortedKeys,
-        function ($acc, $key) use ($before, $after) {
-            if (array_key_exists($key, $before) && array_key_exists($key, $after)) {
-                if ($before[$key] === $after[$key]) {
-                    $acc .= "    $key: $before[$key]\n";
-                } else {
-                    $acc .= "  - $key: $before[$key]\n";
-                    $acc .= "  + $key: $after[$key]\n";
+    return array_map(
+        function ($key) use ($beforeData, $afterData) {
+            if (property_exists($beforeData, $key) && property_exists($afterData, $key)) {
+                if (is_object($beforeData->$key) && is_object($afterData->$key)) {
+                    return [
+                        'key' => $key,
+                        'type' => 'parent',
+                        'children' => getDiffTree($beforeData->$key, $afterData->$key)
+                    ];
                 }
-            } elseif (array_key_exists($key, $before) && !array_key_exists($key, $after)) {
-                $acc .= "  - $key: $before[$key]\n";
-            } else {
-                $acc .= "  + $key: $after[$key]\n";
+                if ($beforeData->$key === $afterData->$key) {
+                    return [
+                        'key' => $key,
+                        'type' => 'unmodified',
+                        'value' => $beforeData->$key,
+                    ];
+                } else {
+                    return [
+                        'key' => $key,
+                        'type' => 'modified',
+                        'oldValue' => $beforeData->$key,
+                        'newValue' => $afterData->$key
+                    ];
+                }
             }
-
-            return $acc;
+            if (property_exists($beforeData, $key) && !property_exists($afterData, $key)) {
+                return [
+                    'key' => $key,
+                    'type' => 'removed',
+                    'value' => $beforeData->$key,
+                ];
+            } else {
+                return [
+                    'key' => $key,
+                    'type' => 'added',
+                    'value' => $afterData->$key
+                ];
+            }
         },
-        ''
+        $sortedKeys
     );
-
-    return "{\n$result}";
 }
 
 function getFileExtension(string $path): string
@@ -64,12 +87,104 @@ function getFileContent(string $path): string
     return $fileData;
 }
 
-function convertBoolValueToString(bool $value): string
+function stylish(array $diffTree, $indent = 0): string
 {
-    return $value ? 'true' : 'false';
+    $indentMap = [
+        'parent' => 4 + $indent,
+        'unmodified' => 4 + $indent,
+        'modified' => 2 + $indent,
+        'added' => 2 + $indent,
+        'removed' => 2 + $indent
+    ];
+
+    $result = array_map(
+        function ($node) use ($indentMap) {
+            $type = $node['type'] ?? null;
+            $key = $node['key'] ?? null;
+            $value = $node['value'] ?? null;
+            $oldValue = $node['oldValue'] ?? null;
+            $newValue = $node['newValue'] ?? null;
+
+            $countedIndent = str_repeat(' ', $indentMap[$type]);
+            $countedEndIndent = str_repeat(' ', $indentMap[$type] + 2);
+
+            if (is_object($value)) {
+                $value = styleInnerObject($value, $indentMap[$type], $countedEndIndent);
+            } else {
+                $value = toString($value);
+            }
+            if (is_object($oldValue)) {
+                $oldValue = styleInnerObject($oldValue, $indentMap[$type], $countedEndIndent);
+            } else {
+                $oldValue = toString($oldValue);
+            }
+            if (is_object($newValue)) {
+                $newValue = styleInnerObject($newValue, $indentMap[$type], $countedEndIndent);
+            } else {
+                $newValue = toString($newValue);
+            }
+
+            switch ($type) {
+                case 'parent':
+                    return "{$countedIndent}$key: {\n" . stylish($node['children'], $indentMap[$type])
+                         . "\n{$countedIndent}}";
+                case 'unmodified':
+                    return "{$countedIndent}$key: $value";
+                case 'modified':
+                    return "{$countedIndent}- $key: $oldValue\n"
+                         . "{$countedIndent}+ $key: $newValue";
+                case 'added':
+                    return "{$countedIndent}+ $key: $value";
+                case 'removed':
+                    return "{$countedIndent}- $key: $value";
+                default:
+                    throw new Exception('Unknown node type.');
+            }
+        },
+        $diffTree
+    );
+
+    return implode("\n", $result);
 }
 
-function convertBoolValues(array $array): array
+function styleInnerObject(object $object, int $indentStart, string $indentEnd): string
 {
-    return array_map(fn($value) => is_bool($value) ? convertBoolValueToString($value) : $value, $array);
+    return "{\n" . stylish(processInnerObject($object), $indentStart + 2) . "\n{$indentEnd}}";
+}
+
+function toString($value): string // TODO type hint
+{
+    if (is_bool($value)) {
+        return $value ? 'true' : 'false';
+    }
+
+    if (is_null($value)) {
+        return 'null';
+    }
+
+    return $value;
+}
+
+function processInnerObject(object $object): array
+{
+    $key = array_keys(get_object_vars($object));
+
+    return array_map(
+        function ($key) use ($object) {
+            if (is_object($object->$key)) {
+                return [
+                    'key' => $key,
+                    'type' => 'parent',
+                    'children' => processInnerObject($object->$key),
+                ];
+            }
+
+            return [
+                'key' => $key,
+                'type' => 'unmodified',
+                'value' => $object->$key,
+            ];
+        },
+        $key
+    );
 }
