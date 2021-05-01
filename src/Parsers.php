4ff59e18338d206a56d7ef317cb98bfda269c651
VK <belsynth@gmail.com>
2021-05-01 12:42:30 +0300

    Изменяет модуль Parsers.php

--- src/Parsers.php
+++ src/Parsers.php
@@ -5,16 +5,14 @@ namespace Differ\Parsers;
 use Exception;
 use Symfony\Component\Yaml\Yaml;
 
-function parse(string $extension, string $content): array
+function parse(string $extension, string $content)
 {
     switch ($extension) {
         case 'json':
-            return json_decode($content, true);
-//            return json_decode($content);
+            return json_decode($content);
         case 'yaml':
         case 'yml':
-            return Yaml::parse($content);
-//            return Yaml::parse($content, Yaml::PARSE_OBJECT_FOR_MAP);
+            return Yaml::parse($content, Yaml::PARSE_OBJECT_FOR_MAP);
         default:
             throw new Exception("Unknown extension \"$extension\"");
     }
