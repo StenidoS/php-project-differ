8bc4cab9a3a8cf539c16208ccb4f290760e25ee7
VK <belsynth@gmail.com>
2021-05-06 11:11:47 +0300

    Изменяет название параметра

--- src/Parsers.php
+++ src/Parsers.php
@@ -6,20 +6,20 @@ use Exception;
 use Symfony\Component\Yaml\Yaml;
 
 /**
- * @param string $extension
+ * @param string $dataType
  * @param string $content
  * @return object
  * @throws Exception
  */
-function parse(string $extension, string $content): object
+function parse(string $dataType, string $content): object
 {
-    switch ($extension) {
+    switch ($dataType) {
         case 'json':
             return json_decode($content);
         case 'yaml':
         case 'yml':
             return Yaml::parse($content, Yaml::PARSE_OBJECT_FOR_MAP);
         default:
-            throw new Exception("Unknown extension \"$extension\"");
+            throw new Exception("Unknown data type \"$dataType\".");
     }
 }
